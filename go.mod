410eebad6bcf53e3002285b243b2b33756562f4c
Nikolay Arhipov <nikolajs.arhipovs@gmail.com>
2019-09-15 21:31:56 +0300

    V1

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module monogame-content
 
 go 1.12
 
-require gopkg.in/Masterminds/sprig.v2 v2.20.0
+require (
+	github.com/Masterminds/goutils v1.1.0 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/huandu/xstrings v1.2.0 // indirect
+	github.com/imdario/mergo v0.3.7 // indirect
+	golang.org/x/crypto v0.0.0-20190911031432-227b76d455e7 // indirect
+	gopkg.in/Masterminds/sprig.v2 v2.20.0
+	gopkg.in/yaml.v2 v2.2.2
+)
--- main.go
+++ main.go
@@ -1,123 +1,70 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/Masterminds/sprig.v2"
 	"io/ioutil"
 	"os"
-	"path/filepath"
-	"regexp"
-	"strings"
 	"text/template"
 )
 
-type Template struct {
-	Path string
-	Body string
-}
-
-type ContentEntry struct {
-	Paths     []string
-	Content   []string
-	FileNames []string
-}
-
-type ConfigFile struct {
-	Templates   []Template
-	Content     []ContentEntry
-	ContentPath string `yaml:"contentPath"`
-}
-
-type ClassField struct {
-	Name  string
-	Value string
-}
-
-var badChars, _ = regexp.Compile(`[^\p{Ll}\p{Lu}\p{Lt}\p{Lo}\p{Nd}\p{Nl}\p{Mn}\p{Mc}\p{Cf}\p{Pc}\p{Lm}]`)
-
 func main() {
-	configFile := flag.String("config", "", "A path to a yaml config file")
+
+	inputTemplate := flag.String("i", "", "Path to gotemplate")
+	outputFile := flag.String("o", "", "Output path")
+	jsonFile := flag.String("d", "", "Path to JSON file with data")
 
 	flag.Parse()
 
-	if configFile == nil || *configFile == "" {
-		println("You must pass -config argument")
+	if inputTemplate == nil || *inputTemplate == "" {
+		println("You must pass -i argument")
+		return
+	}
+	if outputFile == nil || *outputFile == "" {
+		println("You must pass -o argument")
+		return
+	}
+	if jsonFile == nil || *jsonFile == "" {
+		println("You must pass -d argument")
 		return
 	}
 
-	configBytes, err := ioutil.ReadFile(*configFile)
+	templateBytes, err := ioutil.ReadFile(*inputTemplate)
 	if err != nil {
 		panic(err)
 	}
 
-	var config ConfigFile
-	err = yaml.Unmarshal(configBytes, &config)
+	jsonBytes, err := ioutil.ReadFile(*jsonFile)
 	if err != nil {
 		panic(err)
 	}
 
-	var classes = make(map[string][]ClassField)
+	jsonMap := make(map[string]interface{})
+	err = json.Unmarshal(jsonBytes, &jsonMap)
+	if err != nil {
+		panic(err)
+	}
 
-	oldWd, _ := os.Getwd()
-	_ = os.Chdir(filepath.Join(filepath.Dir(*configFile), config.ContentPath))
+	parsedTemplate, err := template.New(*inputTemplate).
+		Funcs(sprig.TxtFuncMap()).
+		Parse(string(templateBytes))
+	if err != nil {
+		panic(err)
+	}
 
-	for i, contentEntry := range config.Content {
-		var fileNames = make([]string, 0, 0)
-		for _, filePattern := range contentEntry.Paths {
-			matches, err := filepath.Glob(filePattern)
-			if err != nil {
-				panic(err)
-			}
-			for _, foundFile := range matches {
-				fileNames = append(fileNames, foundFile)
-			}
-		}
 
-		config.Content[i].FileNames = fileNames
-
-		for _, matchedFile := range fileNames {
-			var dir = filepath.Dir(matchedFile)
-			var segments = strings.Split(dir, string(os.PathSeparator))
-			for i := range segments {
-				segments[i] = strings.Title(segments[i])
-			}
-			var className = strings.Join(segments, "")
-			var fieldName = strings.Title(badChars.ReplaceAllString(strings.SplitN(filepath.Base(matchedFile), ".", 2)[0], ""))
-
-			field := ClassField{
-				Name:  fieldName,
-				Value: strings.TrimSuffix(matchedFile, filepath.Ext(matchedFile)),
-			}
-
-			if _, ok := classes[className]; !ok {
-				classes[className] = []ClassField{field}
-			} else {
-				classes[className] = append(classes[className], field)
-			}
-		}
+	interpolated, err := os.OpenFile(*outputFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		panic(err)
 	}
 
 	var templateData = make(map[string]interface{})
-	templateData["groups"] = config.Content
-	templateData["classes"] = classes
+	templateData["data"] = jsonMap
 
-	_ = os.Chdir(oldWd)
-	_ = os.Chdir(filepath.Dir(*configFile))
-
-	for _, tpl := range config.Templates {
-		parsedTemplate, err := template.New(tpl.Path).Parse(tpl.Body)
-		if err != nil {
-			panic(err)
-		}
-
-		interpolated, err := os.OpenFile(tpl.Path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
-		if err != nil {
-			panic(err)
-		}
-
-		err = parsedTemplate.Execute(interpolated, templateData)
-		if err != nil {
-			panic(err)
-		}
+	err = parsedTemplate.Execute(interpolated, templateData)
+	if err != nil {
+		panic(err)
 	}
+
 }
